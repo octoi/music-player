468a2f5c3146059ea4d4531bb108baadf5083da9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-30 14:40:20 +0530

    load song

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const progressContainer = document.querySelector('.progress-container');
 const title = document.querySelector('#title');
 const cover = document.querySelector('#cover');
 
+// Song Titles
+const songs = ['music_1', 'music_2', 'music_3'];
+
+// Keep track of song
+let songIndex = 2;
+
+// Initial load song
+loadSong(songs[songIndex]);
+
+// Update song details
+
+function loadSong(song) {
+	title.innerTEXT = song;
+	audio.src = `music/${song}.mp3`;
+	cover.src = `images/${song}.png`;
+}
