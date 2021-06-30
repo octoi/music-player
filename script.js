f1be98e20610a43c75453e2041de6ad884e06247
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-30 14:37:17 +0530

    imported stuffs

---
+++ script.js
@@ -0,0 +1,10 @@
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const NextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
