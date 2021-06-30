7bfd77ebffd11c33c16db8e7e1fab5b35cdbba67
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-30 15:03:50 +0530

    progress navigation

--- script.js
+++ script.js
@@ -69,6 +69,14 @@ function updateProgress(e) {
 	progress.style.width = `${progressPercent}%`
 }
 
+function setProgress(e) {
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const duration = audio.duration;
+
+	audio.currentTime = (clickX / width) * duration;
+}
+
 // Event listeners
 playBtn.addEventListener('click', () => {
 	const isPlaying = musicContainer.classList.contains('play');
@@ -85,3 +93,5 @@ prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
 audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress);
